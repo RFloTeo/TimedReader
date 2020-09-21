d1ceec5d65163e9be4528407547733eb5152a243
fr3517 <Unguessablepass99>
2020-09-21 23:57:18 +0100

    Reading from files

--- reader.py
+++ reader.py
@@ -1,16 +1,27 @@
 import random
 import time
+import keyboard
+import sys
 
-print('Enter the words separated by spaces: ', end='')
-in_string = input()
+files = []
+for f in sys.argv[1:]:
+    reader = open(f, 'r')
+    files.append(reader.read())
+    reader.close
+files = list(map((lambda w: w[:-1].split("\n")), files))
 print('How many seconds between words? ', end='')
 delay = float(input())
+key = not delay
 print('Shuffle the words? (Y/N) ', end='')
 shuf = input()
-words = in_string.split(" ")
+words = [e for l in files for e in l]
 if shuf in ['y', 'Y', 'yes', 'Yes']:
     random.shuffle(words)
+print('You have', len(words), 'items')
 for word in words:
     print(word)
-    time.sleep(delay)
+    if key:
+        keyboard.wait('Enter')
+    else:
+        time.sleep(delay)
 print('\nDone!')
